00db685e593bc2b56f29a92adaa0c7a9acfdc0a4
Gilberto Miguel da Silva Santos <gilbertomiguel333@gmail.com>
2025-04-25 19:16:48 +0000

    modificar a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Te convido para o meu batismo, dia 27/04 as 9:00, na igreja além do véu!!</h1>
+    return <h1>Eu te convido para o meu batismo, dia 27/04 as 9:00, na igreja além do véu!!</h1>
 }
 
 export default Home;
